5e4fc3fd1d07bee4a863731be9da5af1b8c66157
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-05-22 23:01:11 +0300

    Added gallery and gallery assets migrations

---
+++ src/database/migrations/2018_05_22_115808_create_hc_gallery_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHcGalleryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hc_gallery', function (Blueprint $table) {
+            $table->increments('count');
+            $table->uuid('id')->unique();
+            $table->datetime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('deleted_at')->nullable();
+
+            $table->string('label');
+            $table->text('description');
+
+            $table->uuid('created_by')->nullable();
+            $table->uuid('cover_id')->nullable();
+
+            $table->boolean('warning_flag')->default(0);
+            $table->text('warning_content')->nullable();
+
+            $table->boolean('show_titles')->default(1);
+            $table->boolean('show_descriptions')->default(1);
+            $table->boolean('hidden')->default(0);
+
+            $table->foreign('created_by')->references('id')->on('hc_user')->onDelete('SET NULL');
+            $table->foreign('cover_id')->references('id')->on('hc_gallery_asset')->onDelete('SET NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hc_gallery');
+    }
+}
---
+++ src/database/migrations/2018_05_22_115810_create_hc_gallery_asset_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHCGalleryAssetTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hc_gallery_asset', function (Blueprint $table) {
+            $table->increments('count');
+            $table->datetime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+
+            $table->uuid('gallery_id');
+            $table->uuid('resource_id');
+            $table->uuid('added_by')->nullable();
+
+            $table->integer('sequence');
+
+            $table->unique(['gallery_id', 'resource_id']);
+
+            $table->string('label')->nullable();
+            $table->text('description')->nullable();
+            $table->text('copyright')->nullable();
+
+            $table->foreign('record_id')->references('id')->on('hc_gallery_category')
+                ->onUpdate('CASCADE')->onDelete('CASCADE');
+
+            $table->foreign('gallery_id')->references('id')->on('hc_gallery')->onDelete('CASCADE');
+            $table->foreign('resource_id')->references('id')->on('hc_resource')->onDelete('CASCADE');
+            $table->foreign('added_by')->references('id')->on('hc_user')->onDelete('SET NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hc_gallery_asset');
+    }
+}
