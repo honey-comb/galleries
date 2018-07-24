62df67000209cc54c8629d0d4eb0d2354306b71d
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-07-24 08:30:56 +0300

    Renamed migration

--- src/database/migrations/2018_07_23_183256_create_hc_gallery_resource_tag_connection_table.php
+++ src/database/migrations/2018_07_23_183256_create_hc_gallery_resource_tag_connection_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateHcResourceTagConnectionTable extends Migration
+class CreateHcGalleryResourceTagConnectionTable extends Migration
 {
     /**
      * Run the migrations.
